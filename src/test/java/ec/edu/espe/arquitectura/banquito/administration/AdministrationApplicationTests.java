665a49dfd4966cd0a20c7134bfea9c51e6607e5e
David Diaz <david3096_diaz@hotmail.com>
2023-08-30 10:25:57 -0500

    Update

---
+++ src/test/java/ec/edu/espe/arquitectura/banquito/administration/AdministrationApplicationTests.java
@@ -0,0 +1,12 @@
+package ec.edu.espe.arquitectura.banquito.administration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdministrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
